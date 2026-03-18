a316dc7d927e53ea820a335c2409fc42588a6cbc
yathishpoojary <yathishpoojary023@gmail.com>
2026-03-19 05:27:15 +0530

    project structure updates

--- Condition.py
+++
@@ -1,2 +0,0 @@
-for i in range(1,29):
-    if i % 2 ==1: print(i)
--- ReverseString.js
+++
@@ -1,7 +0,0 @@
-
-
-let value = "India";
-
-console.log("Original String: ", value);
-
-console.log(" ReverseStinrg ", value.split("").reverse().join(""))
