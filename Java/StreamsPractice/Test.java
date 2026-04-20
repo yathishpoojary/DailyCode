88e1d96bc43b4e55d46de35d542c011ab0ff8bde
yathishpoojary <yathishpoojary023@gmail.com>
2026-04-20 11:25:35 +0530

    practice

--- Java/StreamsPractice/Test.java
+++ Java/StreamsPractice/Test.java
@@ -1,12 +1,17 @@
 package StreamsPractice;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
         String st = "abcbcaabc";
+
+      List<Character>ch=  st.chars().mapToObj(m->(char) m ).collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting())).entrySet().stream().filter(f->f.getValue()> 1).map(Map.Entry::getKey).collect(Collectors.toList());
+
         Map<Character, Long> chr =    st.chars().mapToObj(m->(char) m).collect(Collectors.groupingBy(v->v, Collectors.counting()));
         System.out.println(" chr "+ chr);
     }
