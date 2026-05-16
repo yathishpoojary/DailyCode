156041b4505762e7c9a66046223ac02ac4be91f1
yathishpoojary <yathishpoojary023@gmail.com>
2026-05-16 09:36:31 +0530

    practice

--- Java/May/May16/TestStreamDistinct.java
+++ Java/May/May16/TestStreamDistinct.java
@@ -1,6 +1,22 @@
 package May.May16;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+// Find non-repeated character
 public class TestStreamDistinct {
     public static void main(String[] args) {
+        String st ="swiss";
+
+        Character ch = st.chars().mapToObj(c-> (char) c)
+                .collect(
+                        Collectors.groupingBy(
+                        Function.identity(), LinkedHashMap::new,
+                        Collectors.counting()
+                        )).entrySet().stream().
+                filter(e->e.getValue() ==1).map(Map.Entry::getKey).findFirst().orElse(null);
+           System.out.println(" ch "+ch);
     }
 }
