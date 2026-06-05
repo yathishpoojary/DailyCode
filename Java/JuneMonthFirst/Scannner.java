166af96cdaa7f047b7b9c70c30a0d868f6e7bc7e
yathishpoojary <yathishpoojary023@gmail.com>
2026-06-05 23:46:59 +0530

    practice

--- Java/JuneMonthFirst/Scannner.java
+++ Java/JuneMonthFirst/Scannner.java
@@ -2,6 +2,14 @@ package JuneMonthFirst;
 
 public class Scannner {
     public static void main(String[] args) {
+            String st1 = "Hello";
+            String st2= "Hello";
 
+            String st3 = new String("Hello");
+
+            System.out.println(st1==st2);
+            System.out.println(st1== st3);
+        System.out.println(st1.equals(st3));
+        System.out.println(st2.intern());
     }
 }
